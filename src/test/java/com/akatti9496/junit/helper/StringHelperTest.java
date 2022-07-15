26efc6731d2622c2d8968d5a9ba2e13b3c24368b
a0k04e7 <a0k04e7@homeoffice.wal-mart.com>
2022-07-15 15:11:45 -0500

    pushing the second test case for the system

--- src/test/java/com/akatti9496/junit/helper/StringHelperTest.java
+++ src/test/java/com/akatti9496/junit/helper/StringHelperTest.java
@@ -1,19 +1,27 @@
 package com.akatti9496.junit.helper;
 
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class StringHelperTest {
 
+    StringHelper stringHelper = new StringHelper();
+
     @org.junit.jupiter.api.Test
-    void turncateAInFirst2Positions() {
-        StringHelper stringHelper = new StringHelper();
-        assertAll(()->assertEquals("CD",stringHelper.turncateAInFirst2Positions("AACD")),
-                ()->assertEquals("CD",stringHelper.turncateAInFirst2Positions("ACD")));
+    void turncateAInFirst2PositionsTest() {
+        assertAll("Truncate in the first should return the proper string",()->assertEquals("CD",stringHelper.turncateAInFirst2Positions("AACD")),
+                ()->assertEquals("CD",stringHelper.turncateAInFirst2Positions("ACD")),
+                ()->assertNotEquals("Hello",stringHelper.turncateAInFirst2Positions("ACD")),
+                ()->assertEquals("CDEF",stringHelper.turncateAInFirst2Positions(("CDEF"))));
 
         // the first one is the expected and the second one is the received/actual value
     }
 
     @org.junit.jupiter.api.Test
-    void areFaLTCTS() {
+    void areFaLTCTSTest() {
+        assertEquals(stringHelper.areFaLTCTS("Ab"),true);
+
     }
 }
